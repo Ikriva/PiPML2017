b014a4888e8fd94fe32f68792edbcc2fa9fb778d
Mika Wahlroos <mika.wahlroos@iki.fi>
2017-02-14 01:33:14 +0200

    Add preliminary code for daily data harvesting

---
+++ harvester.py
@@ -0,0 +1,63 @@
+# Data harvester for external data sources in ZooPredict
+# Project in Practical Machine Learning, University of Helsinki, 2017
+#
+# This module provides a command-line tool for harvesting current
+# weather and zoo visitor data from online sources.
+# The command-line harvester should be set to automatically run once a day.
+
+import logging
+import logging.config
+import datetime
+
+from flask import Flask
+
+import models
+import config
+import datafetcher
+
+
+logger = logging.getLogger(__name__)
+
+
+def harvest(app):
+    logger.info("Running data harvester")
+    db = models.db
+    db.init_app(app)
+
+    fmi_api_key = _get_fmi_api_key(app.config['FMI_API_KEY_PATH'])
+    location = app.config['FMI_WEATHER_LOCATION']
+
+    # retrieve FMI weather observations from the previous day
+    # FIXME: should be changed to get forecasts for the next day instead
+
+    offset = datetime.timedelta(days=-1)
+    today = datetime.datetime.now().date()
+
+    date = today + offset
+
+    logger.info("Fetching FMI weather data for {d}".format(d=str(date)))
+    observations = datafetcher.get_daily_fmi_weather_observations(location, date, date, fmi_api_key)
+
+    with app.app_context():
+        for o in observations:
+            db.session.add(o)
+        db.session.commit()
+
+
+def _get_fmi_api_key(api_key_path):
+    logger.debug("Reading API key from {p}".format(p=api_key_path))
+    with open(api_key_path, 'r') as f:
+        api_key = f.read().strip()
+    return api_key
+
+
+def main():
+    """Initializes the database with configuration from the main config file."""
+    logging.config.dictConfig(config.LOGGING_CONF)
+
+    app = Flask(__name__)
+    app.config.from_object("config")
+    harvest(app)
+
+if __name__ == "__main__":
+    main()
