5f3bab9321cec6143ec3088562a059edc526b9bd
Mika Wahlroos <mika.wahlroos@iki.fi>
2017-02-21 21:26:54 +0200

    WIP: use pandas to read and preprocess data

--- tools/modelbuilder.py
+++ tools/modelbuilder.py
@@ -5,21 +5,34 @@
 # Written for the Project in Practical Machine Learning course, 2017,
 # University of Helsinki
 
+import pandas as pd
 import numpy as np
-import numpy.lib.recfunctions as recfunctions
+
+VISITOR_CLASS_LABELS = ["low", "medium", "high"]
+WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 
 def preprocess_weather_data(data):
     pass
 
 
-def main():
-    visitor_data = np.recfromcsv("data/oldVisitorCounts.csv")
-    weather_data = np.recfromcsv("data/weather_observations_jan-mar_2010-2016.csv")
+def preprocess_visitor_data(data):
+    # add visitor count classes to the data frame
+    visitor_classes = pd.qcut(data['visitors'], len(VISITOR_CLASS_LABELS), labels=VISITOR_CLASS_LABELS)
+    data['visitors_class'] = visitor_classes
 
-    recfunctions.join_by('datetime', weather_data, visitor_data)
+    # add visitor counts normalized by weekday
+    visitors_normalized = data.groupby('weekday')['visitors'].transform(lambda x: x / x.mean())
+    data['visitors_normalized'] = visitors_normalized
+
+
+def main():
+    visitor_data = pd.read_csv("data/oldVisitorCounts.csv")
+    weather_data = pd.read_csv("data/weather_observations_jan-mar_2010-2016.csv")
 
     preprocess_weather_data(weather_data.data)
+
+    full_data = pd.merge(visitor_data, weather_data, on='datetime')
 
 
 if __name__ == "__main__":
