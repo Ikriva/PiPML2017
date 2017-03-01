f08307c0f0f78cbe5c67487a5760462a1a169274
Mika Wahlroos <mika.wahlroos@iki.fi>
2017-03-02 01:54:07 +0200

    Display prediction in the UI even if no observed value is available yet

--- models.py
+++ models.py
@@ -135,11 +135,15 @@ class Classifier(PredictionModel):
 def get_zoo_predictions_and_actuals():
     """
     Returns a list of all visitor statistic predictions and actual values.
+    If a prediction for a given date exists but an actual value is not available,
+    the value for the ZooStatisticActual will be None.
     @return: list of (ZooStatisticPrediction, ZooStatisticActual) tuples
     """
-    results = db.session.query(ZooStatisticPrediction, ZooStatisticActual)\
-                        .join(ZooStatisticActual,
-                              ZooStatisticPrediction.date == ZooStatisticActual.date)\
+
+    results = db.session.query(ZooStatisticPrediction)\
+                        .outerjoin(ZooStatisticActual,
+                                   ZooStatisticPrediction.date == ZooStatisticActual.date)\
                         .order_by(ZooStatisticPrediction.date.desc())\
+                        .add_entity(ZooStatisticActual)\
                         .all()
     return results
